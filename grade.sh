c1028e1d413c2f3a01124f0df58ac8286a6edc92
user <user@users-MacBook-Air-2.local>
2024-05-28 10:09:34 -0700

    Modified grade.sh

--- grade.sh
+++ grade.sh
@@ -14,3 +14,24 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if ! [[ -f student-submission/ListExamples.java ]]; then
+echo 'Could not find ListExamples.java in the submitted directory.'
+exit
+fi
+
+cp *.java grading-area
+cp student-submission/*.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+javac -cp $CPATH Server.java GradeServer.java ListExamples.java TestListExamples.java
+if [[ $? -ne 0 ]]; then
+javac -cp $CPATH Server.java GradeServer.java ListExamples.java TestListExamples.java 2> CompileError.txt
+echo 'Did not compile, see CompileError.txt for more info'
+exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > TestListExamplesOutput.txt
+echo 'All tests ran, check TestListExamplesOutput.txt for results'
